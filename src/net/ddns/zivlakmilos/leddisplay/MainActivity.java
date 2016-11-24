a341a101a3d8dc61db755d4bd2a4138f4b591bdd
Milos Zivlak <zivlakmilos@gmail.com>
2016-11-24 23:27:12 +0100

    Basic fade out/in animation on button connect

--- src/net/ddns/zivlakmilos/leddisplay/MainActivity.java
+++ src/net/ddns/zivlakmilos/leddisplay/MainActivity.java
@@ -1,16 +1,43 @@
 package net.ddns.zivlakmilos.leddisplay;
 
 import android.support.v7.app.AppCompatActivity;
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+	
+	public static int FadeAnimationDuration = 500;
+	
+	private LinearLayout m_layoutConnection;
+	private LinearLayout m_layoutCommunication;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		m_layoutConnection =
+				(LinearLayout)findViewById(R.id.layoutConnection);
+		m_layoutCommunication =
+				(LinearLayout)findViewById(R.id.layoutCommunication);
+		
+		m_layoutCommunication.setVisibility(View.GONE);
+		
+		Button btnConnect = (Button)findViewById(R.id.btnConnect);
+		btnConnect.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View sender) {
+				showCommunication();
+			}
+		});
 	}
 
 	@Override
@@ -30,5 +57,28 @@ public class MainActivity extends AppCompatActivity {
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
+	}
+	
+	private void showConnection() {
+	}
+	
+	private void showCommunication() {
+		m_layoutCommunication.setAlpha(0.0f);
+		m_layoutConnection.setAlpha(1.0f);
+		
+		m_layoutConnection.animate()
+						  .alpha(0.0f)
+						  .setDuration(FadeAnimationDuration)
+						  .setListener(new AnimatorListenerAdapter() {
+							
+							@Override
+							public void onAnimationEnd(Animator animation) {
+								m_layoutConnection.setVisibility(View.GONE);
+								m_layoutCommunication.setVisibility(View.VISIBLE);
+								m_layoutCommunication.animate()
+													 .alpha(1.0f)
+													 .setDuration(FadeAnimationDuration);
+							}
+						});
 	}
 }
